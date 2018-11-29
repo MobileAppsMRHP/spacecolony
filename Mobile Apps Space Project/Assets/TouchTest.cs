787ae2a8e3d1da065f97e1bfb9ca16eff02b8aa1
radiantwanderer <radiantgamewanderer@gmail.com>
2018-11-28 18:45:10 -0600

    Home build
    The one I have at home that updated to the version we're using.

---
+++ Mobile Apps Space Project/Assets/TouchTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchTest : MonoBehaviour {
+
+    
+	// Use this for initialization
+	void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.touchCount > 0)
+        {
+            gameObject.GetComponent<Animator>().enabled = !gameObject.GetComponent<Animator>().enabled;
+        }
+	}
+}
