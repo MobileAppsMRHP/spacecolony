b06d676b7bc3f1d952aa3f94a2d92e320d95dfac
D211\budak7273 <budak7273@students.d211.org>
2018-12-13 10:52:58 -0600

    Add comments to touchtest

--- UnityProject/Mobile Apps Space Project/Assets/TouchTest.cs
+++ UnityProject/Mobile Apps Space Project/Assets/TouchTest.cs
@@ -14,15 +14,15 @@ public class TouchTest : MonoBehaviour {
 	void Update () {
 		if (Input.touchCount > 0)
         {
-            gameObject.GetComponent<Animator>().enabled = !gameObject.GetComponent<Animator>().enabled;
+            gameObject.GetComponent<Animator>().enabled = !gameObject.GetComponent<Animator>().enabled; //toggle animator enabled status
         }
 
-        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began) //if there are touch events in the buffer to process...
         {
-            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint((Input.GetTouch(0).position)), new Vector2(0f, 0f));
-            if (hit.collider != null)
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint((Input.GetTouch(0).position)), new Vector2(0f, 0f)); //do a raycast to see what they hit
+            if (hit.collider != null) //if it hit something, anything, ....
             {
-                Debug.Log("Touched it");
+                Debug.Log("Touched it"); //log that something was hit by the touch event
             }
         }
     }
