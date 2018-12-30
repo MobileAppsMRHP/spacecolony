50ea3c115d0ecf7ff6947f211688ec8a19db7f07
radiantwanderer <radiantgamewanderer@gmail.com>
2018-12-30 17:17:36 -0600

    Pan and Zoom
    I made the panning and zooming with some code I got from a tutorial.

---
+++ UnityProject/Mobile Apps Space Project/Assets/CamPan.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamPan : MonoBehaviour {
+    bool zooming;
+    Vector3 touchInitialLocation;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        zooming = GetComponent<CamZoom>().isZooming;
+        if (Input.touchCount == 1 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            touchInitialLocation = Camera.main.ScreenToWorldPoint((Input.GetTouch(0).position));
+        }
+
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved && !zooming)
+        {
+            Vector3 changeDirection = touchInitialLocation - Camera.main.ScreenToWorldPoint((Input.GetTouch(0).position));
+            Camera.main.transform.position += changeDirection;
+        }
+
+    }
+}
---
+++ UnityProject/Mobile Apps Space Project/Assets/CamZoom.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamZoom : MonoBehaviour {
+    public float zoomMin;
+    public float zoomMax;
+    public float zoomRate;
+    public bool isZooming;
+    Vector3[] touchInitialLocation = new Vector3[2];
+    
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.touchCount == 2)
+        {
+            isZooming = true;
+            Touch touchZero = Input.GetTouch(0);
+            Touch touchOne = Input.GetTouch(1);
+
+            Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
+            Vector2 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
+
+            float prevMagnitude = (touchZeroPrevPos - touchOnePrevPos).magnitude;
+            float currentMagnitude = (touchZero.position - touchOne.position).magnitude;
+
+            float change = currentMagnitude - prevMagnitude;
+
+            zoom(change*zoomRate);
+        }
+        if (Input.touchCount == 0)
+        {
+            isZooming = false;
+        }
+    }
+
+    void zoom(float increment)
+    {
+        Camera.main.orthographicSize = Mathf.Clamp(Camera.main.orthographicSize - increment, zoomMin, zoomMax);
+    }
+        
+}
