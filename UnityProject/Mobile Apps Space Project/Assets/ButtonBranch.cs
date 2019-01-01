299d748cf83a52eab00478979d1f3676099dadc2
radiantwanderer <radiantgamewanderer@gmail.com>
2019-01-01 12:25:16 -0600

    Update ButtonBranch.cs

--- UnityProject/Mobile Apps Space Project/Assets/ButtonBranch.cs
+++ UnityProject/Mobile Apps Space Project/Assets/ButtonBranch.cs
@@ -1,8 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class ButtonBranch : MonoBehaviour {
+    public class ButtonScale
+    {
+        enum ScaleMode { MatchWidthHeight, IndependentWidthHeight}
+        ScaleMode mode;
+        Vector2 referenceButtonSize;
+
+        [HideInInspector]
+        public Vector2 referenceScreenSize;
+        public Vector2 newButtonSize;
+
+        public void Initialize(Vector2 refButtonSize, Vector2 refScreenSize, int scaleMode)
+        {
+            mode = (ScaleMode)scaleMode;
+            referenceButtonSize = refButtonSize;
+            referenceScreenSize = refScreenSize;
+            SetNewButtonSize();
+        }
+
+        void SetNewButtonSize()
+        {
+            newButtonSize.x = (referenceButtonSize.x * Screen.width) / referenceScreenSize.x;
+            if (mode == ScaleMode.MatchWidthHeight)
+            {
+                newButtonSize.y = newButtonSize.x;
+            }
+            else if (mode == ScaleMode.IndependentWidthHeight)
+            {
+                newButtonSize.y = (referenceButtonSize.y * Screen.height) / referenceScreenSize.y;
+            }
+        }
+    }
+
 
 	// Use this for initialization
 	void Start () {
