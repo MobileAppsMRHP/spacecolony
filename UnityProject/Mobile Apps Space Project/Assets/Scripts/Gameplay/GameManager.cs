570184eef844cedc1e6279c07bfde796c9d4bdec
radiantwanderer <radiantgamewanderer@gmail.com>
2019-01-09 11:05:47 -0600

    a

---
+++ UnityProject/Mobile Apps Space Project/Assets/Scripts/Gameplay/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager instance = null; //singleton pattern
+	// Use this for initialization
+	void Awake () { //Only initializes once
+		if (instance == null)
+        {
+            instance = this;
+        }
+        else if(instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
