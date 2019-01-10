1c8725c1d7936b9e31888f471fe2a0608e9dbeff
radiantwanderer <radiantgamewanderer@gmail.com>
2019-01-10 11:01:05 -0600

    Import firebase

--- UnityProject/Mobile Apps Space Project/Assets/Scripts/Gameplay/Loader.cs
+++ UnityProject/Mobile Apps Space Project/Assets/Scripts/Gameplay/Loader.cs
@@ -8,7 +8,7 @@ public class Loader : MonoBehaviour
     // Use this for initialization
     void Awake()
     {
-        if (gameManager.instance == null)
+        if (GameManager.instance == null)
         {
             Instantiate(gameManager);
         }
