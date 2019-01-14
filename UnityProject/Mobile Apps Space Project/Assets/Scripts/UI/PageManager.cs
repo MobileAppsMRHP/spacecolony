4eaa9bbe9e6aa6196d2657a7c00136f02ff77cfd
radiantwanderer <radiantgamewanderer@gmail.com>
2019-01-14 11:04:18 -0600

    UI page change

---
+++ UnityProject/Mobile Apps Space Project/Assets/Scripts/UI/PageManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PageManager : MonoBehaviour {
+    public GameObject[] pages = new GameObject[4];
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void swapPages(int current, int next)
+    {
+        pages[current].SetActive(false);
+        pages[next].SetActive(true);
+    }
+}
