02a5503119812a6a314cab789c2a1ec4b7f9bfc0
radiantwanderer <radiantgamewanderer@gmail.com>
2019-01-15 10:56:24 -0600

    Gameplay and UI

---
+++ UnityProject/Mobile Apps Space Project/Assets/Scripts/Gameplay/Crew.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crew : MonoBehaviour {
+    string name;
+    string role;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public Crew(string name, string role)
+    {
+        this.name = name;
+        this.role = role;
+
+    }
+
+    public string getName()
+    {
+        return name;
+    }
+
+    public string getRole()
+    {
+        return role;
+    }
+
+    public void changeRole(string newRole)
+    {
+        role = newRole;
+    }
+}
--- UnityProject/Mobile Apps Space Project/Assets/Scripts/Gameplay/GameManager.cs
+++ UnityProject/Mobile Apps Space Project/Assets/Scripts/Gameplay/GameManager.cs
@@ -3,8 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour {
+    public List<Crew> crewMembers;
+    public float food;
+    public float energy;
+    public float water;
+    public float money;
 
     public static GameManager instance = null; //singleton pattern
+
 	// Use this for initialization
 	void Awake () { //Only initializes once
 		if (instance == null)
