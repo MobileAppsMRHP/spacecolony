9eab7e31887ca26a01af8edda00e8853f780c24c
radiantwanderer <radiantgamewanderer@gmail.com>
2019-01-24 11:06:23 -0600

    Drag and Drop

---
+++ UnityProject/Mobile Apps Space Project/Assets/Scripts/UI/DragAndDrop.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragAndDrop : MonoBehaviour//, IDragHandler,  IEndDragHandler
+{ 
+    private bool selected;
+
+    /*public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = Input.mousePosition;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        transform.localPosition = Vector3.zero;
+    }*/
+
+    // Use this for initialization
+    void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (selected)
+        {
+            Vector2 cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            transform.position = new Vector2(cursorPos.x, cursorPos.y);
+        }
+	}
+
+    void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            selected = true;
+        }
+    }
+}
