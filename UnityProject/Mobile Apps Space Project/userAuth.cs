a3ff2b493cf15cea78e44f05a5688a0247c28092
hwishn <45208429+hwishn@users.noreply.github.com>
2019-01-24 10:50:29 -0600

    added basic methods to userAuthentication script

--- UnityProject/Mobile Apps Space Project/userAuth.cs
+++ UnityProject/Mobile Apps Space Project/userAuth.cs
@@ -5,12 +5,45 @@ public class UserAuth
 
     public static void main()
     {
-    Firebase.Auth.FirebaseAuth auth = Firebase.Auth.FirebaseAuth.DefaultInstance;
+    Firebase.Auth.FirebaseAuth auth = Firebase.Auth.FirebaseAuth.DefaultInstance;// initializes Firebase.Auth class and allows access to all class APIs 
 
+       
+    }
 
+    public static void Authenticate()//trades google id token for firebase credential and authenticates user with firebase. Uses code from https://firebase.google.com/docs/auth/unity/google-signin?authuser=1
+    {
+        Firebase.Auth.Credential credential =
+    Firebase.Auth.GoogleAuthProvider.GetCredential(googleIdToken, googleAccessToken);
+        auth.SignInWithCredentialAsync(credential).ContinueWith(task => {
+            if (task.IsCanceled)
+            {
+                Debug.LogError("SignInWithCredentialAsync was canceled.");
+                return;
+            }
+            if (task.IsFaulted)
+            {
+                Debug.LogError("SignInWithCredentialAsync encountered an error: " + task.Exception);
+                return;
+            }
 
+            Firebase.Auth.FirebaseUser newUser = task.Result;
+            Debug.LogFormat("User signed in successfully: {0} ({1})",
+                newUser.DisplayName, newUser.UserId);
+        });
+     }
 
-
-
-}
+    public static firebaseUser getUserInfo() //gets basic user info from database. Uses code from https://firebase.google.com/docs/auth/unity/google-signin?authuser=1
+    {
+        Firebase.Auth.FirebaseUser user = auth.CurrentUser;
+        if (user != null)
+        {
+            string name = user.DisplayName;
+            string email = user.Email;
+            System.Uri photo_url = user.PhotoUrl;
+            // The user's Id, unique to the Firebase project.
+            // Do NOT use this value to authenticate with your backend server, if you
+            // have one; use User.TokenAsync() instead.
+            string uid = user.UserId;
+        }
+    }
 }
