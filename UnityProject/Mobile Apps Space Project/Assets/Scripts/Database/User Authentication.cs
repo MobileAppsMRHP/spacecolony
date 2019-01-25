95cc494aa2b72359203f7597ec3e51c51d3716c4
hwishn <45208429+hwishn@users.noreply.github.com>
2019-01-25 10:45:16 -0600

    messed around with userAuthentication script

--- UnityProject/Mobile Apps Space Project/Assets/Scripts/Database/User Authentication.cs
+++ UnityProject/Mobile Apps Space Project/Assets/Scripts/Database/User Authentication.cs
@@ -6,11 +6,37 @@ public class UserAuthentication : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
-	}
+        Firebase.Auth.FirebaseAuth auth = Firebase.Auth.FirebaseAuth.DefaultInstance;//import API thorugh default instance of class 
+    }
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    void Authenticate()//trades in google token for firebase credentials. made with help from https://firebase.google.com/docs/auth/unity/google-signin?authuser=0
+    {
+        Firebase.Auth.Credential credential =
+    Firebase.Auth.GoogleAuthProvider.GetCredential(googleIdToken, googleAccessToken);
+        auth.SignInWithCredentialAsync(credential).ContinueWith(task => {
+            if (task.IsCanceled)
+            {
+                Debug.LogError("SignInWithCredentialAsync was canceled.");
+                return;
+            }
+            if (task.IsFaulted)
+            {
+                Debug.LogError("SignInWithCredentialAsync encountered an error: " + task.Exception);
+                return;
+            }
+
+            Firebase.Auth.FirebaseUser newUser = task.Result;
+            Debug.LogFormat("User signed in successfully: {0} ({1})",
+                newUser.DisplayName, newUser.UserId);
+        });
+    }
+     void signOut(Firebase.Auth.FirebaseUser user)//signs out user 
+    {
+        user.SignOut();
+    }
 }
