426cf435d4fc1e890d45298528c9964e42fb498a
Toningbasher <45208445+Toningbasher@users.noreply.github.com>
2019-02-04 11:05:46 -0600

    B

---
+++ UnityProject/Mobile Apps Space Project/Assets/Scripts/UI/ChangeButton.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeButton : MonoBehaviour {
+
+    public Text textShowed = null;
+    public bool tracker = true;
+
+    public void ChangeWordNotification()
+    {
+        if (tracker)
+        {
+            textShowed.text = "Off";
+            tracker = false;
+        }
+        else
+        {
+            textShowed.text = "On";
+            tracker = true;
+        }
+    }
+
+    public void ChangeWordStatus()
+    {
+        if (tracker)
+        {
+            textShowed.text = "Logged Off";
+            tracker = false;
+        }
+        else
+        {
+            textShowed.text = "Logged On";
+            tracker = true;
+        }
+    }
+}
