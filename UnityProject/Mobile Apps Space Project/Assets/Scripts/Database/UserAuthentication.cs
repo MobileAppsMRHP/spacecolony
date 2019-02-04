249bd992b068eacf84fc8ebcc929e249909a4b1e
D211\budak7273 <budak7273@students.d211.org>
2019-02-04 10:37:53 -0600

    Put in temporary auth creds to stop errors

--- UnityProject/Mobile Apps Space Project/Assets/Scripts/Database/UserAuthentication.cs
+++ UnityProject/Mobile Apps Space Project/Assets/Scripts/Database/UserAuthentication.cs
@@ -17,24 +17,24 @@ public class UserAuthentication : MonoBehaviour {
     public void Authenticate()//trades in google token for firebase credentials. made with help from https://firebase.google.com/docs/auth/unity/google-signin?authuser=0
     {
         Firebase.Auth.FirebaseAuth auth = Firebase.Auth.FirebaseAuth.DefaultInstance;//import API thorugh default instance of class 
-        Firebase.Auth.Credential credential =
-    Firebase.Auth.GoogleAuthProvider.GetCredential(googleIdToken, googleAccessToken);
-        auth.SignInWithCredentialAsync(credential).ContinueWith(task => {
-            if (task.IsCanceled)
-            {
-                Debug.LogError("SignInWithCredentialAsync was canceled.");
-                return;
-            }
-            if (task.IsFaulted)
-            {
-                Debug.LogError("SignInWithCredentialAsync encountered an error: " + task.Exception);
-                return;
-            }
+        Firebase.Auth.Credential credential = //REPLACE THESE ACCESS TOKENS LATER
+            Firebase.Auth.GoogleAuthProvider.GetCredential("googleIdToken", "googleAccessToken");
+            auth.SignInWithCredentialAsync(credential).ContinueWith(task => {
+                if (task.IsCanceled)
+                {
+                    Debug.LogError("SignInWithCredentialAsync was canceled.");
+                    return;
+                }
+                if (task.IsFaulted)
+                {
+                    Debug.LogError("SignInWithCredentialAsync encountered an error: " + task.Exception);
+                    return;
+                }
 
-            Firebase.Auth.FirebaseUser newUser = task.Result;
-            Debug.LogFormat("User signed in successfully: {0} ({1})",
-                newUser.DisplayName, newUser.UserId);
-        });
+                Firebase.Auth.FirebaseUser newUser = task.Result;
+                Debug.LogFormat("User signed in successfully: {0} ({1})",
+                    newUser.DisplayName, newUser.UserId);
+            });
     }
      public void signOut( Firebase.Auth.FirebaseAuth auth)//signs out user 
     {
