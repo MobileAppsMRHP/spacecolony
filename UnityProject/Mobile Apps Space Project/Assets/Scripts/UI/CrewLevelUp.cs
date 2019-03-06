c4960e9b77ff2d625239e44585743cfe1c49bc00
radiantwanderer <radiantgamewanderer@gmail.com>
2019-03-06 11:05:01 -0600

    SkillPoints UI Script

--- UnityProject/Mobile Apps Space Project/Assets/Scripts/UI/CrewLevelUp.cs
+++ UnityProject/Mobile Apps Space Project/Assets/Scripts/UI/CrewLevelUp.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class CrewLevelUp : MonoBehaviour {
     public GameObject gameManager;
+    public Crew selectedCrew;
+    int crewNum;
 	// Use this for initialization
 	void Start () {
-		
+        selectedCrew = gameManager.GetComponent<GameManager>().CrewMembers[0];
+        crewNum = 0;
 	}
 	
 	// Update is called once per frame
@@ -14,8 +17,21 @@ public class CrewLevelUp : MonoBehaviour {
 		
 	}
 
-    public void IncreaseSkill()
+    public void SwitchCrew(bool right)
     {
+        if (right)
+        {
+            crewNum = crewNum == gameManager.GetComponent<GameManager>().CrewMembers.Count - 1 ? 0 : crewNum + 1;
+        }
+        else
+        {
+            crewNum = crewNum == 0 ? gameManager.GetComponent<GameManager>().CrewMembers.Count - 1 : crewNum - 1;
+        }
+        selectedCrew = gameManager.GetComponent<GameManager>().CrewMembers[crewNum];
+    }
 
+    public void IncreaseStat(int num)
+    {
+        selectedCrew.IncreaseSkill(num);
     }
 }
