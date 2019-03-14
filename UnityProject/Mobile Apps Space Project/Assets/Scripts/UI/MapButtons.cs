76b4418a41045f43a0f1f19ee0708cb5238e95c5
Toningbasher <45208445+Toningbasher@users.noreply.github.com>
2019-03-14 11:05:21 -0500

    Working on Zoom

---
+++ UnityProject/Mobile Apps Space Project/Assets/Scripts/UI/MapButtons.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapButtons : MonoBehaviour {
+
+    public Camera Cam;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	public void Zoom () {
+        Cam.fieldOfView = 60;
+	}
+}
