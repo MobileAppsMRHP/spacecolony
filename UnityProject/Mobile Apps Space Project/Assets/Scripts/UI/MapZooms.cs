580772c7a84fb9bc5a49d3c3582882f4495ee114
Toningbasher <45208445+Toningbasher@users.noreply.github.com>
2019-04-15 11:05:26 -0500

    CAM ZOOM FIN WORKS!!!!!!!!

--- UnityProject/Mobile Apps Space Project/Assets/Scripts/UI/MapZooms.cs
+++ UnityProject/Mobile Apps Space Project/Assets/Scripts/UI/MapZooms.cs
@@ -7,6 +7,8 @@ public class MapZooms : MonoBehaviour {
     public Camera MainCamera;
     public GameObject TargetPosition;
     public int speed = 2;
+    public int camx;
+    public int camy;
     bool camera_move_enabled = false;
 
     void Update()
@@ -22,8 +24,8 @@ public class MapZooms : MonoBehaviour {
 
     public void UserClickedCameraResetButton()
     {
-        TargetPosition.transform.position = new Vector3(-106.2617f, 68.81419f, 14.92558f);
-        TargetPosition.transform.rotation = Quaternion.Euler(39.7415f, 145.0724f, 0);
+        TargetPosition.transform.position = new Vector3(camx, camy, 0f);
+        TargetPosition.transform.rotation = Quaternion.Euler(0, 0, 0);
         camera_move_enabled = true;
     }
 }
