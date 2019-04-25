0c6f7262b1dd58ccbfa155b101f96be9e7f4393b
Rob B <budak7273@students.d211.org>
2019-04-25 11:05:18 -0500

    Remove Start Button dependence on UserAuthentication

--- UnityProject/Mobile Apps Space Project/Assets/Scripts/UI/StartButton.cs
+++ UnityProject/Mobile Apps Space Project/Assets/Scripts/UI/StartButton.cs
@@ -5,22 +5,24 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class StartButton : MonoBehaviour {
-    protected bool loggedIn;
-    protected static UserAuthentication auth;
+    //protected bool loggedIn;
+    //protected static UserAuthentication auth;
     // Use this for initialization
     void Start () {
-        auth = gameObject.AddComponent<UserAuthentication>();
-        loggedIn = auth.UserLoggedIn();
+        //auth = gameObject.AddComponent<UserAuthentication>();
+        
     }
 
     public void ChangeScreen()
     {
-        if (loggedIn == true)
+        if (PlayerPrefs.HasKey("UserAuthToken"))
         {
+            Debug.Log("PlayerPrefs has a token, going to gameplay screen.");
             SceneManager.LoadScene("01Gameplay");
         }
         else 
         {
+            Debug.Log("PlayerPrefs does NOT have a token, going to login screen.");
             SceneManager.LoadScene("login");
 
         }
